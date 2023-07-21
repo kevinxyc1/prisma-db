cc1cc83c25d210310a7c6666ce8491d6329fa80b
Kevin Xu <kevinxu820@gmail.com>
2023-07-20 21:03:56 -0700

    initial commit

---
+++ script.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      username: 'Kev',
+      publicKey: 'HhUBgs7AQVdaCag9aEyYchRWTSnZCMsfZvuwb89xYTjB',
+    },
+  })
+  console.log(user)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
